975e8a72e4691588991dfcbf4199e2287ade31e1
cleveng <cleveng@gmail.com>
2024-10-07 18:05:53 +0800

    perf: login with account,add api type

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,13 @@
 import { Text, View } from '@tarojs/components'
 import Taro, { ENV_TYPE, useLoad } from '@tarojs/taro'
 
-import { Button, Divider, Form, Input, NavBar, SafeArea, Space } from '@nutui/nutui-react-taro'
+import { API } from '#/types/api'
+import { Button, Cell, Divider, Form, Input, NavBar, Notify, SafeArea, Space } from '@nutui/nutui-react-taro'
+
+import { NotifyType } from '@nutui/nutui-react-taro/dist/types/packages/notify/notify.taro'
+import { useState } from 'react'
+
+import { useUserStore } from '@/store/user'
 
 export default function Index() {
   useLoad(() => {
@@ -16,33 +22,51 @@ export default function Index() {
     })
   }
 
-  const loginWithGoogle = async () => {
-    Taro.showToast({ title: '登录成功', icon: 'none' })
+  const submitFailed = error => {
+    Taro.showToast({ title: JSON.stringify(error), icon: 'error' })
+  }
+
+  const [isVisible, SetIsVisible] = useState(false)
+  const [states, SetStates] = useState({
+    message: '',
+    type: 'danger' as NotifyType
+  })
+  const login = useUserStore(state => state.login)
+  const loginWithAccount = async (params: Record<string, string>) => {
+    try {
+      const res = await Taro.request({
+        url: '/auth/login?app=ds',
+        data: {
+          ...params,
+          payload: null
+        },
+        method: 'POST'
+      })
+
+      const { data } = res as unknown as API.Response<string>
+      SetStates({
+        message: '登录成功',
+        type: 'success'
+      })
+      SetIsVisible(true)
+      login(data)
+    } catch (e) {
+      const { message } = e as API.Response<string>
+      console.log(message)
+      SetStates({
+        message,
+        type: 'danger'
+      })
+      SetIsVisible(true)
+    }
   }
 
   const loginWithWechat = async () => {
-    Taro.showToast({ title: '开始请求', icon: 'none' })
-    try {
-      const res = await Taro.login()
-      try {
-        const value = await Taro.request({
-          url: 'https://www.awish.vip/auth/wechat/callback?app=ds',
-          data: {
-            state: '',
-            code: res.code
-          },
-          header: {
-            appid: 'dsc6805f597d8c411e'
-          },
-          method: 'POST'
-        })
-        console.log(value)
-      } catch (error) {
-        console.log(error)
-      }
-    } catch (error) {
-      console.log(error)
-    }
+    Taro.showToast({ title: '微信快捷登录', icon: 'none' })
+  }
+
+  const loginWithGoogle = async () => {
+    Taro.showToast({ title: '谷歌快捷登录', icon: 'none' })
   }
 
   return (
@@ -60,29 +84,43 @@ export default function Index() {
           登录
         </NavBar>
       )}
-      <View className={`mx-auto flex w-full max-w-[520px] flex-col justify-between overflow-auto p-1`}>
+      <Notify
+        id='notify'
+        visible={isVisible}
+        type={states.type}
+        onClose={() => {
+          SetIsVisible(false)
+        }}
+        onClick={() => {
+          console.log('click')
+        }}
+      >
+        {states.message}
+      </Notify>
+      <View className={`mx-auto flex min-h-[100vh-50px] w-full flex-col justify-between overflow-auto bg-gray-50 p-1`}>
         <View className='mt-5'>
           <Form
+            divider
             labelPosition='top'
             starPosition='left'
-            className='p-0'
+            onFinish={values => loginWithAccount(values)}
+            onFinishFailed={(_, errors) => submitFailed(errors)}
             footer={
               <>
-                <Button block type='info' onClick={loginWithWechat}>
+                <Button size='large' block nativeType='submit' type='info'>
                   登录
                 </Button>
               </>
             }
           >
-            <Form.Item required label='用户名' name='account' rules={[{ required: true, message: '请输入用户名' }]}>
+            <Form.Item required label='用户名' name='username' rules={[{ required: true, message: '请输入用户名' }]}>
               <Input placeholder='请输入用户名' type='text' />
             </Form.Item>
-            <Divider />
             <Form.Item
               label='密码'
               name='password'
               rules={[
-                { max: 15, message: '密码长度不能超过15个字符' },
+                { max: 15, min: 6, message: '密码长度不能超过15个字符' },
                 { required: true, message: '请输入密码' }
               ]}
             >
@@ -93,15 +131,17 @@ export default function Index() {
 
         <View className='mb-5'>
           <Divider>或者</Divider>
-          <Space direction='vertical' className='mx-auto space-y-2 text-center text-xs text-gray-500'>
-            <Button size='large' type='success' block>
-              微信授权登录
-            </Button>
-            {isWeb && (
+          <Space direction='vertical' className='mx-auto space-y-2 text-center text-xs'>
+            <Cell className='bg-gray-50 shadow-none'>
+              <Button size='large' type='success' block onClick={loginWithWechat}>
+                微信授权登录
+              </Button>
+            </Cell>
+            <Cell className='bg-gray-50 shadow-none'>
               <Button size='large' type='primary' block onClick={() => loginWithGoogle()}>
                 谷歌授权登录
               </Button>
-            )}
+            </Cell>
           </Space>
         </View>
       </View>
